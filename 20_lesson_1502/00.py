f3fef606b7e23a08c44e49e85594a62dd5ecb825
demidraksin <greatraksin@icloud.com>
2022-02-15 19:04:27 +0300

    Finishing platform game

--- 20_lesson_1502/00.py
+++ 20_lesson_1502/00.py
@@ -26,12 +26,27 @@ def show_text(text, color, x, y):
 
 
 finished = False  # флаг, который отвечает за работу программы
+pause = False
 while not finished:  # пока игра не окончена
     clock.tick(30)  # частота обновления 30 кадров в секунду
     # отслеживаю события (нажатия кнопок)
     for event in pg.event.get():  # для каждого события в списке событий
         if event.type == pg.QUIT:  # если нажали на крестик
             finished = True
+
+    while pause:  # пока игра стоит "на паузе"
+        screen.fill((255, 255, 255))
+        show_text('Press C to continue of ESC to exit', (0, 0, 0), W // 2, H // 2)
+        pg.display.update()
+
+        # цикл обработки событий в паузе
+        for pause_event in pg.event.get():
+            if pause_event.type == pg.KEYDOWN:
+                if pause_event.key == pg.K_ESCAPE:  # если нажали ESC, завершить игру
+                    pg.quit()
+                elif pause_event.key == pg.K_c:  # если нажали С, "начать игру заново"
+                    score = 3
+                    pause = False
 
     screen.fill((255, 255, 255))
 
@@ -43,6 +58,11 @@ while not finished:  # пока игра не окончена
         enemy_y = 0
         enemy_x = randint(0, W)
         score += 1
+
+    if platform.right >= W:
+        player_x = W - 200
+    elif platform.left <= 0:
+        player_x = 0
 
     show_text(f'Score: {score}', (0, 0, 0), 0, 0)
     pg.display.update()
@@ -60,4 +80,8 @@ while not finished:  # пока игра не окончена
         enemy_y = 0
         enemy_x = randint(0, W)
         score -= 1
+
+    # проигрыш
+    if score <= 0:
+        pause = True
 
