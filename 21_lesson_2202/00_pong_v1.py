2b72db31b3e70a0e232a7591501f151eacc88bba
demidraksin <greatraksin@icloud.com>
2022-02-22 18:36:54 +0300

    Adding ball motion function

--- 21_lesson_2202/00_pong_v1.py
+++ 21_lesson_2202/00_pong_v1.py
@@ -1,6 +1,31 @@
 import pygame as pg
 from pygame.draw import rect, circle, polygon, aaline, ellipse
 
+
+def ball_motion(obj, width, height, plr, enm):
+    """
+    Функция движения мяча по игровому полю
+    :param obj: игровой объект-мяч
+    :param width: ширина экрана
+    :param height: высота экрана
+    :param plr: игровая ракетка
+    :param enm: ракетка-враг
+    :return: None
+    """
+    global ball_speed_x, ball_speed_y
+
+    obj.x += ball_speed_x
+    obj.y += ball_speed_y
+
+    if obj.top <= 0 or obj.bottom > height:  # если мяч ударился об нижнюю или верхнюю часть экрана
+        ball_speed_y *= -1  # направить его в противоположную сторону
+    elif obj.left <= 0 or obj.right > width:  # если мяч ударился об правую или левую границу экрана
+        ball_speed_x *= -1  # направить его в противоположную сторону
+
+    if obj.colliderect(plr) or obj.colliderect(enm):  # если мяч коснулся игрока или врага
+        ball_speed_x *= - 1  # отбить мяч в другую сторону
+
+
 W = 1280
 H = 720
 screen = pg.display.set_mode((W, H))
@@ -16,12 +41,20 @@ ball = pg.Rect(W // 2 - 15, H // 2 - 15, 30, 30)
 player = pg.Rect(W - 20, H // 2, 10, 150)
 opponent = pg.Rect(10, H // 2, 10, 150)
 
+# Game variables
+speed = 8
+ball_speed_x = speed
+ball_speed_y = speed
+
 finished = False
 while not finished:
     clock.tick(30)
     for event in pg.event.get():
         if event.type == pg.QUIT:
             finished = True
+
+    # Game logic
+    ball_motion(ball, W, H, player, opponent)
 
     # Visuals
     screen.fill(bg_color)
