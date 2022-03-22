1d9d5ea9e4b3307e6f8e461b9861a5d6d44305e0
demidraksin <greatraksin@icloud.com>
2022-03-22 18:33:26 +0300

    Adding paddle motion

--- 23_lesson_1503/03_relative_points.py
+++ 23_lesson_1503/03_relative_points.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.draw import rect, circle, polygon
+import random as r
 
 
 def moving(obj, speed, l_side, r_side):
@@ -36,6 +37,10 @@ road.center = W // 2, H // 2
 border_left = pg.Rect(road.left - W * 0.1, 0, W * 0.1, H)
 border_right = pg.Rect(road.right, 0, W * 0.1, H)
 
+road_width = road.width  # измеряет ширину дороги
+paddle1 = pg.Rect(road.x, -50, road_width * 0.3, 30)  # препятствие №1
+paddle2 = pg.Rect(road.x, -H // 2, road_width * 0.3, 30)  # препятствие №2
+
 img = pg.image.load('car.png').convert_alpha()  # загружаю картинку машины
 img_rect = img.get_rect()  # превращаю картинку в объект
 center = W // 2, H * 0.85  # указываю координаты появления машины
@@ -45,6 +50,7 @@ car_rect = img_rect  # клон объекта
 
 car_speed = 0  # начальная скорость машины
 angle = 0  # начальный градус поворота машины
+speed = 10  # общее значение скорости для всех объектов в игре
 
 finished = False
 while not finished:
@@ -54,17 +60,17 @@ while not finished:
             finished = True
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_RIGHT:
-                car_speed += 10
+                car_speed += speed
                 angle = -10
             if event.key == pg.K_LEFT:
-                car_speed -= 10
+                car_speed -= speed
                 angle = 10
             car = pg.transform.rotate(img, angle)  # поворачиваю картинку на заданный угол
         if event.type == pg.KEYUP:  # если кнопку отпустили
             if event.key == pg.K_RIGHT:
-                car_speed -= 10
+                car_speed -= speed
             if event.key == pg.K_LEFT:
-                car_speed += 10
+                car_speed += speed
             angle = 0  # угол 0
             car = pg.transform.rotate(img, angle)  # поворачиваю картинку на заданный угол
 
@@ -73,12 +79,36 @@ while not finished:
     rect(screen, GRAY, road)  # дорога
     rect(screen, SAND, border_left)  # левая обочина
     rect(screen, SAND, border_right)  # правая обочина
-    rect(screen, WHITE, [396, 0, 8, H])  # линия разметки
+    rect(screen, WHITE, [(W // 2) - (W * 0.01), 0, W * 0.02, H])  # линия разметки
+    rect(screen, RED, paddle1)  # препятствие №1
+    rect(screen, RED, paddle2)  # препятствие №2
 
     screen.blit(car, car_rect)  # отображаю машину в координатах car_rect
-    rect(screen, RED, car_rect, 1)
 
     pg.display.update()
 
     # Game logic
     moving(car_rect, car_speed, border_left, border_right)
+
+    paddle1.y += speed  # препятствие падает вниз
+    paddle2.y += speed  # препятствие падает вниз
+    if paddle1.y >= H:  # если препятствие ушло за пределы экрана
+        x = r.randint(1, 3)
+        if x == 1:
+            paddle1.x = road.x  # ставлю препятствие слева (по левому краю дороги)
+        elif x == 2:
+            paddle1.center = road.center  # ставлю препятствие посередине
+        else:
+            paddle1.right = road.right  # ставлю препятствие справа (по правому краю дороги)
+        paddle1.y = -50  # отправляю препятствие наверх
+
+    if paddle2.y >= H:  # если препятствие ушло за пределы экрана
+        x = r.randint(1, 3)
+        if x == 1:
+            paddle2.x = road.x  # ставлю препятствие слева (по левому краю дороги)
+        elif x == 2:
+            paddle2.center = road.center  # ставлю препятствие посередине
+        else:
+            paddle2.right = road.right  # ставлю препятствие справа (по правому краю дороги)
+        paddle2.y = -100  # отправляю препятствие наверх
+
