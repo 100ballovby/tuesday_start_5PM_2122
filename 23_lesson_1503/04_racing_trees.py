eaf24cde30a7c1f607ef8d2800ba991281a7c256
demidraksin <greatraksin@icloud.com>
2022-03-22 19:02:29 +0300

    Adding trees and paddles collision

--- 23_lesson_1503/04_racing_trees.py
+++ 23_lesson_1503/04_racing_trees.py
@@ -41,12 +41,26 @@ road_width = road.width  # измеряет ширину дороги
 paddle1 = pg.Rect(road.x, -50, road_width * 0.3, 30)  # препятствие №1
 paddle2 = pg.Rect(road.x, -H // 2, road_width * 0.3, 30)  # препятствие №2
 
+# Images
 img = pg.image.load('car.png').convert_alpha()  # загружаю картинку машины
 img_rect = img.get_rect()  # превращаю картинку в объект
 center = W // 2, H * 0.85  # указываю координаты появления машины
 img_rect.center = center  # ставлю машину в координаты
 car = img  # клон картинки
 car_rect = img_rect  # клон объекта
+
+tree1 = pg.image.load('tree-3.png').convert_alpha()
+tree1_rect = tree1.get_rect()
+tree2 = pg.image.load('tree-3.png').convert_alpha()
+tree2_rect = tree2.get_rect()
+
+tree1 = pg.transform.scale(tree1, [W * 0.1, H * 0.2])  # ширина 10% от ширины экрана, высота - 20% от высоты экрана
+# scale(что_меняем, [ширина, высота])
+tree2 = pg.transform.scale(tree2, [W * 0.1, H * 0.2])
+tree1_rect.x = 10
+tree1_rect.y = -(H * 0.2)
+tree2_rect.x = W - 90
+tree2_rect.y = -(H * 0.8)
 
 car_speed = 0  # начальная скорость машины
 angle = 0  # начальный градус поворота машины
@@ -84,7 +98,8 @@ while not finished:
     rect(screen, RED, paddle2)  # препятствие №2
 
     screen.blit(car, car_rect)  # отображаю машину в координатах car_rect
-
+    screen.blit(tree1, tree1_rect)
+    screen.blit(tree2, tree2_rect)
     pg.display.update()
 
     # Game logic
@@ -112,3 +127,13 @@ while not finished:
             paddle2.right = road.right  # ставлю препятствие справа (по правому краю дороги)
         paddle2.y = -100  # отправляю препятствие наверх
 
+    tree1_rect.y += speed  # дерево летит вниз
+    tree2_rect.y += speed  # дерево летит вниз
+    if tree1_rect.y >= H:  # если у дерева ушел за пределы экрана
+        tree1_rect.y = -(H * 0.2)  # поднять дерево наверх
+    if tree2_rect.y >= H:  # если у дерева ушел за пределы экрана
+        tree2_rect.y = -(H * 0.8)  # поднять дерево наверх
+
+    # Game over
+    if car_rect.colliderect(paddle1) or car_rect.colliderect(paddle2):
+        finished = True
